c5efbca2abcb79068118a0b17767bbd30b31743b
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-07-20 14:14:13 +0530

    links added

--- front-end/src/Components/Section-3.jsx
+++ front-end/src/Components/Section-3.jsx
@@ -5,50 +5,34 @@ function Section3() {
     <section className="other-api-services">
       <h3 className="head">Other useful JSON files</h3>
       <ul>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <li className="item item-1">
-            
-          </li>
+        <a href="/unsplash-images" target="_blank" rel="noopener noreferrer">
+          <li className="item item-1"></li>
         </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <li className="item item-2">
-            
-          </li>
+        <a href="/todos" target="_blank" rel="noopener noreferrer">
+          <li className="item item-2"></li>
         </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <li className="item item-3">
-            
-          </li>
+        <a href="/tweets" target="_blank" rel="noopener noreferrer">
+          <li className="item item-3"></li>
         </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <li className="item item-4">
-            
-          </li>
+        <a href="/nasa-apod" target="_blank" rel="noopener noreferrer">
+          <li className="item item-4"></li>
         </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <li className="item item-5">
-            
-          </li>
+        <a href="/nasa-mars" target="_blank" rel="noopener noreferrer">
+          <li className="item item-5"></li>
         </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <li className="item item-6">
-            
-          </li>
+        <a href="/covid-world" target="_blank" rel="noopener noreferrer">
+          <li className="item item-6"></li>
         </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <li className="item item-7">
-            
-          </li>
+        <a href="/covid-india" target="_blank" rel="noopener noreferrer">
+          <li className="item item-7"></li>
         </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <li className="item item-8">
-            
-          </li>
+        <a href="/covid-usa" target="_blank" rel="noopener noreferrer">
+          <li className="item item-8"></li>
         </a>
       </ul>
 
       <div className="contact-me">
-        Looking for something else?{" "}
+        Looking for something else?
         <a href="mailto:devangsaklani@gmail.com">Contact me</a>
       </div>
     </section>
